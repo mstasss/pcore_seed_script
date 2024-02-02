d0561360b582c0f0d8b4e21d13c999695bee76ec
Ricky Bunn <bunnrf@gmail.com>
2024-02-02 15:23:37 -0500

    Vendor example from UI to Ruby

--- app/services/project_seeder.rb
+++ app/services/project_seeder.rb
@@ -4,8 +4,15 @@ class ProjectSeeder
     @project_id = project_id
   end
 
-  def run
+  def run(data)
+    # here we should have all the data from the UI
+    # we can use the Procore API client to create a vendor
+    #
+    # vendor = procore_client.create_vendor(data[:vendor][:name])
+    # OR
     # vendor = procore_client.create_vendor(RandomNameGenerator.generate_fake_name)
+    #
+    # we can potentially use the created vendor in following steps
   end
 
   private
